b1aea864a483be410976f3baf5d7988d52254db9
Ucal Efendiyev <ucal.efendiyev@mail.ru>
2022-11-21 23:46:16 +0400

    homeworkblank

---
+++ blank.py
@@ -0,0 +1,13 @@
+#print('z'in'pyhton')
+#if 'z' in 'pyhton':
+     #print("Phytonda 'z' herfi var")
+#else:
+     #print("Pytonda 'z' herfi yoxdur")  
+#qiymet=12
+#print(type(qiymet/2))
+#if 12%3 :
+    #print('bolunmur')
+#else:
+    #print('bolunur')     
+
+
