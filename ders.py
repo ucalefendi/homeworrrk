52ced4f3fe670671c9f82d73c07d4b8ae8a1f2e9
Ucal Efendiyev <ucal.efendiyev@mail.ru>
2022-11-24 10:40:10 +0400

    lesson

---
+++ ders.py
@@ -0,0 +1,14 @@
+#bal={'adil':48,'arif':12,'ayten':89}
+#print(bal['adil'])
+#print(bal['ayten'])
+#a='5'
+#b=int(a)*3
+#print(b)
+#a=40
+#b=int(a)/2
+#print(b)
+#qiymet=float(input('qiymeti daxil et'))
+#endirimfaizi=float(input('endirim faizini daxil et'))
+#yekunqiymet=qiymet-qiymet*endirimfaizi/100
+#print(yekunqiymet)
+
---
+++ lesson.py
@@ -0,0 +1,37 @@
+#val=5382173992019017 
+#val='5382173992019017'[8:].rjust(16, '*')
+#print(val)
+
+
+#print(divmod(15,4))
+#print(3**3)
+
+
+#print(round(256.91872, 4))
+#print(round(256.91872, -1))
+
+#val='0'*2 +'34'
+#print(val)
+
+#seria='aze7891360'
+#val=str(seria)
+#if  val!= "aze7891360" :
+    # print ("vesiqenin seriyasi yanlisdir")
+#else :
+     #print("vesiqenin seriyasi dogrudur")   
+
+#7ci tapsiriq
+#x=168
+#if x%8==0 and x%3==0 and x%7==0:
+      #print('duzdur') 
+#else:
+       #print('sehfdir')     
+
+
+
+ 
+
+
+
+
+
