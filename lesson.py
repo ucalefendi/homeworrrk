c12070a348fcee9ebd537815c8627475af0b581a
Ucal Efendiyev <ucal.efendiyev@mail.ru>
2022-11-28 00:14:17 +0400

    'lesson1'

--- lesson.py
+++ lesson.py
@@ -28,6 +28,46 @@
        #print('sehfdir')     
 
 
+#cumle='azerbaycan respublikasi'
+#val=cumle.upper()
+#print(val)
+
+
+#val='azerbaycan'[:4].ljust(16,'9')
+#print(val)
+
+
+#eded=float(input('edede girin'))
+#eded=int(eded)
+#eded=str(eded)
+#print(len(eded))
+
+
+#amount=int(input('ne qeder kredit lazimdir'))
+
+#if amount<2000 :
+     #print('2000 altina kredit verilmir!')
+#elif amount<10000:
+    #print('yekun borc',amount*1.05,'olacaq!') 
+#elif amount<50000:
+     #print('yekun borc',amount*1.04,'olacaq!')    
+#elif amount<200000:
+     #print('yekun borc',amount*1.03,'olacaq')  
+#elif amount<500000:
+     #print('yekun borc',amount*1.02,'olacaq!') 
+#else:
+     #print('500000 den yuxari kredit verilmir!')     
+
+#print(dir(str))  
+
+
+
+
+
+
+
+
+
 
  
 
---
+++ lesson1.py
@@ -0,0 +1,15 @@
+login=str(input('enter code'))
+if len(login) = [8] :
+     print('parol dogrudur!') 
+if len(login) <= [40] :
+     print('parol dogrudur!')        
+elif login.isascii is true :
+     print('parol dogrudur!')     
+elif login.isalnum is true :
+     print('parol dogrudur') 
+elif login.islower or login.isupper:
+     print('bir kicik ve bir herf olmalidir!')  
+elif login.isalpha or login.isnumeric:
+     print('mutleq minimum bir herf ve minimum bir reqem olmalidir!')  
+else:
+     print('kod uygun deyil!') 
