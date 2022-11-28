f495e1d277822fc94ca4f9165c19a1ea821fde48
Ucal Efendiyev <ucal.efendiyev@mail.ru>
2022-11-28 22:28:28 +0400

    calisma

--- lesson1.py
+++ lesson1.py
@@ -1,15 +1,18 @@
-login=str(input('enter code'))
-if len(login) = [8] :
-     print('parol dogrudur!') 
-if len(login) <= [40] :
-     print('parol dogrudur!')        
-elif login.isascii is true :
-     print('parol dogrudur!')     
-elif login.isalnum is true :
-     print('parol dogrudur') 
-elif login.islower or login.isupper:
-     print('bir kicik ve bir herf olmalidir!')  
-elif login.isalpha or login.isnumeric:
-     print('mutleq minimum bir herf ve minimum bir reqem olmalidir!')  
-else:
-     print('kod uygun deyil!') 
+#login=str(input('enter code'))        
+#if login.isascii is true :
+     #print('parol dogrudur!')     
+#elif login.isalnum is true :
+     #print('parol dogrudur') 
+# login.islower or login.isupper:
+     #print('bir kicik ve bir herf olmalidir!')  
+#elif login.isalpha or login.isnumeric:
+     #print('mutleq minimum bir herf ve minimum bir reqem olmalidir!')  
+#else:
+     
+
+
+
+
+
+x=str(input('siteni daxil edin'))
+print(x.removeprefix('http://').removesuffix('.com'))
