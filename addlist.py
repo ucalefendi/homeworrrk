1d9e14476a9b87e6b97e93d9c4377bdaf5400a8b
Ucal Efendiyev <ucal.efendiyev@mail.ru>
2022-12-13 22:35:23 +0400

    calisma'

---
+++ addlist.py
@@ -0,0 +1,58 @@
+# r=[1,2,3,4,58]
+# del r[0]
+# print(r)
+
+#say=0
+# count=0.1
+# for i in range(0,31) :
+#     if i > 0:
+#         print(count)
+#         count = 2*count
+# print(count)        
+    
+# d = [1,2,3,4,5,5,'country','seneqal',123]
+# print(all(d))
+
+# alphabets = {'a', 'b', 'c'}
+# #value for the dictionary
+# number = 1
+# luget=dict.fromkeys(alphabets,number)
+# print(luget)
+
+# python = {'age': 20,'capital':'baku','pul':'azn','zaman':24 }
+# print(python)
+
+# python['age'] = 30
+# print(python)
+
+# python.update({'capital':'karakas','pul':'dollar'})
+# print(python)
+# python['ad'] = 'ucal'
+# print(python)
+
+# # for x in python:
+# #     print(python[x])
+# print(python.keys())
+# print(python.items())
+
+# text = 'pyhton is good program '
+# print(text.split(''))
+
+# print(dir(str))
+# print(dir({}))
+# 2. İstifadəçinin girdiyi cümlədəki sözləri tərsinə çevirilmiş şəkildə istifadəçiyə qaytarın. Örnək:
+# Input: `This is an example!` 
+# Output: `sihT si na !elpmaxe`
+
+# input('')
+# print(tinput[ : : -1])
+
+# words = ['dəftər','kitab','qələm','karandaş','əlcək','pencək','əmək']
+
+# #result = [word.replace('ə','e') for word in words if 'ə' in word]
+# #print(result)
+
+# result = { word:word.replace('ə','e') for word in words if 'ə' in word}
+# print(result)
+
+
