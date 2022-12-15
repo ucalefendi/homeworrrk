7bac678e27a972281a7858d3d6574a059dd1fa10
Ucal Efendiyev <ucal.efendiyev@mail.ru>
2022-12-15 11:46:20 +0400

    list-dictionary

---
+++ luget.py
@@ -0,0 +1,46 @@
+# # l = ['ucal','efendiyev']
+# # first_name, last_name = l
+# #print(first_name,last_name) 
+
+
+# l = [['ucal','efendi'],['cavad','salman'],['arif','qurbanov']]
+
+# for ad,soyad in l:
+#     print(ad,soyad)
+
+# 3. `[2384, 12385, 13226, 653, 12362423]` list içərisindəki ədədlərin key olduğu və
+#  value-ların həmin ədədlərin rəqəm sayı olduğu bir dictionary hazırlayın
+# number = [2384, 12385, 13226, 653, 12362423]
+
+# number_str =[str(n) for n in number]
+# print(number_str)
+# numberlist= {i:len(i) for i in number_str}
+# print(numberlist)
+#print(dir(str))
+
+# 4. -100-dən müsbət 100-ə qədər ədədlər arasında 7-yə bölünən 
+# ədədlərin 3-ə vurulmasından ibarət bir list qurun.
+#  Bunun üçün range və list comprehensions istifadə edin.
+
+# numbers = [n*3 for n in range(-100,101) if n%7 == 0]
+# print(numbers)
+
+# 1. Istifadəçi sizə bir input vasitəsilə bunun kimi bir məlumat girəcək:
+# `input: firstname Elcin, username elchina, birthday 18-08-2000`
+# Bu inputa əsasən yuxarıdakı dictionary-ni update edin ve istifadəçiye gosterin.
+#  Misal yuxarıdakı inputa əsasən dictionary son halı aşağıdakı kimi olacaq.
+# user_info = {
+#     'firstname': 'Elcin',
+#     'lastname': 'Huseynov',
+#     'username': 'elchina',
+#     'password': '12345',
+#     'birthday': '18-08-2000'
+# }
+data_input = {'firstname': Elcin, 'username': 'elchina',' birthday': '18-08-2000'}
+data_input2 = {'firstname': Elcin, 'lastname': 'Huseynov',' username': 'elcina''18-08-2000'}
+    
+
+
+
+
+
